706abcbae97d01d684bce0152ccc44234fadaee8
trickyrat <wangjian0513@outlook.com>
2021-09-20 23:14:40 +0800

    Add 283. Move Zeroes

--- solution.py
+++ solution.py
@@ -219,6 +219,18 @@ class Solution:
                 if start == current:
                     break
 
+    def moveZeroes(self, nums: List[int]):
+        """
+        283. Move Zeroes
+        """
+        n = len(nums)
+        left = right = 0
+        while right < n:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+
     def search_v1(self, nums: List[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
