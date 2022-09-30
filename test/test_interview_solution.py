4f672fe9947d69773713dfda099728ad8d209d07
trickyrat <wangjian0513@outlook.com>
2022-09-30 20:34:10 +0800

    test: add get_kth_magic_number test cases

--- test/test_interview_solution.py
+++ test/test_interview_solution.py
@@ -15,3 +15,11 @@ class TestInterviewSolution:
     def test_is_flipped_string(self, s1: str, s2: str, expected: bool):
         actual = self.solution.is_flipped_string(s1, s2)
         assert actual == expected
+
+    @pytest.mark.parametrize(
+        "k, expected",
+        [(5, 9)],
+    )
+    def test_get_kth_magic_number(self, k: int, expected: int):
+        actual = self.solution.get_kth_magic_number(k)
+        assert actual == expected
