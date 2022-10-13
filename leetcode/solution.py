35c549885fe4bdbf17bea5c3f899f18708f931b4
trickyrat <wangjian0513@outlook.com>
2022-10-14 00:53:54 +0800

    feature: add 801. Minimum Swaps To Make Sequences Increasing

--- leetcode/solution.py
+++ leetcode/solution.py
@@ -948,6 +948,21 @@ class Solution:
 
         return nx(k + 1) - nx(k)
 
+    def min_swap(self, nums1: List[int], nums2: List[int]) -> int:
+        """801. Minimum Swaps To Make Sequences Increasing"""
+        n = len(nums1)
+        a, b = 0, 1
+        for i in range(1, n):
+            at, bt = a, b
+            a = b = n
+            if nums1[i] > nums1[i - 1] and nums2[i] > nums2[i - 1]:
+                a = min(a, at)
+                b = min(b, bt + 1)
+            if nums1[i] > nums2[i - 1] and nums2[i] > nums1[i - 1]:
+                a = min(a, bt)
+                b = min(b, at + 1)
+        return min(a, b)
+
     def unique_morse_representations(self, words: List[str]) -> int:
         """804. Unique Morse Code Words"""
         morse = [
