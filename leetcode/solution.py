fa6247c1a4260b8e0f001d908c8af9a2c8e4e393
trickyrat <wangjian0513@outlook.com>
2022-10-25 23:55:53 +0800

    feature: add 934. Shortest Bridge

--- leetcode/solution.py
+++ leetcode/solution.py
@@ -1256,6 +1256,38 @@ class Solution:
             return [first + length - 1, second + length]
         return [-1, -1]
 
+    def shortest_bridge(self, grid: List[List[int]]) -> int:
+        """934. Shortest Bridge"""
+        n = len(grid)
+        for i, row in enumerate(grid):
+            for j, col in enumerate(row):
+                if col != 1:
+                    continue
+                q = []
+
+                def dfs(x: int, y: int) -> None:
+                    grid[x][y] = -1
+                    q.append((x, y))
+                    for nx, ny in (x+1,y), (x-1,y),(x, y+1),(x,y-1):
+                        if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 1:
+                            dfs(nx, ny)
+
+                dfs(i, j)
+                step = 0
+                while True:
+                    temp = q
+                    q = []
+                    for x, y in temp:
+                        for nx, ny in (x+1,y), (x-1,y),(x, y+1),(x,y-1):
+                            if 0 <= nx < n and 0 <= ny < n:
+                                if grid[nx][ny] == 1:
+                                    return step
+                                if grid[nx][ny] == 0:
+                                    grid[nx][ny] = -1
+                                    q.append((nx, ny))
+                    step += 1
+        return 0
+
     def distinct_subseq_ii(self, s: str) -> int:
         """940. Distinct Subsequences II"""
         mod = 10**9 + 7
