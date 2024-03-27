ac61178a1d3eeb33c681bd2c7d4fcf26fbd29872
trickyrat <wangjian0513@outlook.com>
2024-03-27 22:51:56 +0800

    feat: add 2580. Count Ways to Group Overlapping Ranges

--- src/leetcode/solution.py
+++ src/leetcode/solution.py
@@ -2472,3 +2472,20 @@ class Solution:
             if ch in seen:
                 return ch
             seen.add(ch)
+
+    def count_ways(self, ranges: List[List[int]]) -> int:
+        """2580. Count Ways to Group Overlapping Ranges"""
+        MOD = 10**9 + 7
+        ranges.sort()
+        n = len(ranges)
+        res = 1
+        i = 0
+        while i < n:
+            r = ranges[i][1]
+            j = i + 1
+            while j < n and ranges[j][0] <= r:
+                r = max(r, ranges[j][1])
+                j += 1
+            res = (res * 2) % MOD
+            i = j
+        return res
