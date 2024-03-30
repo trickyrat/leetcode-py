9bb476d0fe1ffde260772b071971614a1d577d6e
trickyrat <wangjian0513@outlook.com>
2024-03-30 13:49:25 +0800

    chore: format codes

--- src/leetcode/foo.py
+++ src/leetcode/foo.py
@@ -8,15 +8,15 @@ class Foo:
         self.firstJobDone.acquire()
         self.secondJobDone.acquire()
 
-    def first(self, print_first: 'Callable[[], None]') -> None:
+    def first(self, print_first: "Callable[[], None]") -> None:
         print_first()
         self.firstJobDone.release()
 
-    def second(self, print_second: 'Callable[[], None]') -> None:
+    def second(self, print_second: "Callable[[], None]") -> None:
         with self.firstJobDone:
             print_second()
             self.secondJobDone.release()
 
-    def third(self, print_third: 'Callable[[], None]') -> None:
+    def third(self, print_third: "Callable[[], None]") -> None:
         with self.secondJobDone:
             print_third()
