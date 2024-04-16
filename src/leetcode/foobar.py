1097fc718f521a10717b3be65bccd6c7e73fa04b
trickyrat <wangjian0513@outlook.com>
2024-04-16 21:57:25 +0800

    feat: Add 1115. Print FooBar Alternately

---
+++ src/leetcode/foobar.py
@@ -0,0 +1,22 @@
+from threading import Lock
+from typing import Callable
+
+
+class Foobar:
+    def __init__(self, n: int) -> None:
+        self.n = n
+        self.FooLock = Lock()
+        self.BarLock = Lock()
+        self.FooLock.acquire()
+
+    def foo(self, print_foo: "Callable[[], None]") -> None:
+        for i in range(self.n):
+            self.FooLock.acquire()
+            print_foo()
+            self.BarLock.release()
+
+    def bar(self, print_bar: "Callable[[], None]") -> None:
+        for i in range(self.n):
+            self.BarLock.acquire()
+            print_bar()
+            self.FooLock.release()
