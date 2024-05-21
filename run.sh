a19ee7a549d384506245a46b307f6717d7498d82
trickyrat <wangjian0513@outlook.com>
2024-05-21 20:34:02 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
+echo "activating virtual environment"
 . ./.venv/bin/activate
+echo "deactivating virtual environment"
 
+echo "running tests"
 coverage run -m pytest .
+echo "tests complete"
 
+echo "generating test results"
 coverage html -d ./test_results
+echo "test results complete"
 
+echo "deactivating virtual environment"
 deactivate
+echo "virtual environment deactivated"
