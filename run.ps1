b6d735e3f11e84bb92ec6ace5170152efaacace9
trickyrat <wangjian0513@outlook.com>
2024-05-21 20:29:55 +0800

    upadte run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,15 @@
+Write-Host "Activating virtual environment..."
 ./.venv/Scripts/activate.ps1
+Write-Host "Virtual environment activated."
 
+Write-Host "Running tests..."
 coverage run -m pytest .
+Write-Host  "Tests completed."
 
+Write-Host "Generating test results..."
 coverage html -d ./test_results
+Write-Host "Test results generated."
+
+Write-Host "Deactivating virtual environment..."
+./.venv/Scripts/deactivate.ps1
+Write-Host "Virtual environment deactivated."
